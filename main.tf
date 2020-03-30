2973e0b0b818e49e8e082caf9bf82503da37291f
Shea Phillips <shea.phillips@gmail.com>
2020-03-30 16:40:06 -0700

    moving files around removing SANs

--- main.tf
+++ main.tf
@@ -13,9 +13,7 @@ resource "acme_registration" "reg" {
 
 resource "acme_certificate" "certificate" {
   account_key_pem           =acme_registration.reg.account_key_pem
-  common_name               = "cloud.brightcoast.com"
-  subject_alternative_names = ["gcp.brightcoast.com", "aws.brightcoast.ca", "az.brightcoast.ca"]
-
+  common_name               = "cloud.brightcoast.ca"
   dns_challenge {
     provider = "route53"
   }
